575b153ec1a5dc1a18d2c47310bcf403b0eb7c3c
ktrung306 <khactrungcc18@gmai.com>
2024-03-16 20:01:13 +0700

    Lab_ReactNative2

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import React from "react";
 import Demo_Lab1 from "./src/screens/Lab1_Demo";
 import Lab1_Bai1 from "./src/screens/Lab1_Bai1";
 import Lab1_Bai3 from "./src/screens/Lab1_Bai3";
+import CounterStart from "./src/screens/Lab2_Demo";
+import { Lab2_Bai1 } from "./src/screens/Lab2_Bai1/Main";
+import Lab2_Bai2_Bai3 from "./src/screens/Lab2_Bai2_Bai3/Main";
 
 const App = () => {
   return (
-   <Lab1_Bai1></Lab1_Bai1>
+   <Lab2_Bai2_Bai3></Lab2_Bai2_Bai3>
   );
 };
 export default App;  
