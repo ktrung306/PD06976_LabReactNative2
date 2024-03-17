af51056350c4ce0db70958a5e51f1e748dc96cb3
ktrung306 <khactrungcc18@gmai.com>
2024-03-17 19:27:18 +0700

    Lab_ReactNative

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Demo_Lab1 from "./src/screens/Lab1_Demo";
 import Lab1_Bai1 from "./src/screens/Lab1_Bai1";
+import Lab1_Bai2 from "./src/screens/Lab1_Bai2";
 import Lab1_Bai3 from "./src/screens/Lab1_Bai3";
 import CounterStart from "./src/screens/Lab2_Demo";
 import { Lab2_Bai1 } from "./src/screens/Lab2_Bai1/Main";
